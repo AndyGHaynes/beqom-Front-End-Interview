695e76ffbc4914f360e9cc9a8dc9ba13a3342016
Andy Haynes <andyghaynes@gmail.com>
2017-02-19 17:38:13 -0800

    video player modal

--- components/Video/Video.js
+++ components/Video/Video.js
@@ -1,38 +1,61 @@
 import React from 'react';
 import s from './Video.css';
+import VideoPlayer from '../VideoPlayer';
 
-export default ({ video }) => (
-  <div className={s.video}>
-    <div className={s.title}>
-      {video.title}
-    </div>
-    <img
-      className={s.preview}
-      src={video.pic}
-      width={240}
-      height={180}
-    />
-    <div className={s.play}>
-      {/*
-        SVG icon from Material icon set
-        https://material.io/icons/#ic_play_circle_outline
-      */}
-      <svg
-        fill="#000000"
-        height="80"
-        viewBox="0 0 24 24"
-        width="80"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 0h24v24H0z" fill="none"/>
-        <path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
-      </svg>
-    </div>
-    <div className={s.description}>
-      {video.description}
-    </div>
-    <div className={s.uploaded}>
-      {video.time}
-    </div>
-  </div>
-);
+class Video extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    };
+  }
+
+  openModal = () => this.setState({ modalOpen: true });
+  closeModal = () => this.setState({ modalOpen: false });
+
+  render() {
+    const { video } = this.props;
+    return (
+      <div className={s.video} onClick={this.openModal}>
+        <div className={s.title}>
+          {video.title}
+        </div>
+        <img
+          className={s.preview}
+          src={video.pic}
+          width={240}
+          height={180}
+        />
+        <div className={s.play}>
+          {/*
+            SVG icon from Material icon set
+            https://material.io/icons/#ic_play_circle_outline
+          */}
+          <svg
+            fill="#000000"
+            height="80"
+            viewBox="0 0 24 24"
+            width="80"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 0h24v24H0z" fill="none"/>
+            <path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+          </svg>
+        </div>
+        <div className={s.description}>
+          {video.description}
+        </div>
+        <div className={s.uploaded}>
+          {video.time}
+        </div>
+        <VideoPlayer
+          video={video}
+          modalOpen={this.state.modalOpen}
+          closeModal={this.closeModal}
+        />
+      </div>
+    );
+  }
+}
+
+export default Video;
---
+++ components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+
+export default ({ video, modalOpen, closeModal }) => (
+  <Modal show={modalOpen} onHide={closeModal}>
+    <Modal.Header>
+      <Modal.Title>
+        {video.title}
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <div>
+        <iframe
+          width={560}
+          height={315}
+          src={video.url.replace('watch?v=', 'embed/')}
+          frameBorder={0}
+          allowFullScreen
+        />
+      </div>
+      <div>
+        {video.description}
+      </div>
+    </Modal.Body>
+    <Modal.Footer>
+      <div>
+        {video.time}
+      </div>
+    </Modal.Footer>
+  </Modal>
+);
